a8db6e5ffc6b71a044d058bf0a3875a49c34d2ea
Glauber <glauber.bannwart@gmail.com>
2023-06-06 13:58:27 -0300

    Data Storage Implementation Fixed

--- app.js
+++ app.js
@@ -1,60 +1,53 @@
 const addBook = document.querySelector('.book-form');
 const bookShelf = document.querySelector('.items');
 
-const bookCollection = [];
-localStorage.setItem('bookCollection', JSON.stringify(bookCollection))
+const store = localStorage.getItem('bookCollection');
+const bookCollection = JSON.parse(store) || [];
 
-const store = localStorage.getItem('bookCollection')
+const renderBooks = () => {
+  bookShelf.innerHTML = '';
 
-
-
-
-// create a function to render the book
-const generateBook = (bookTitle, bookAuthor) => {
-  const html = ` 
- <article class="book-list">
-        <ul class="book-details" >
-            <li class="title-list">
-        <span>
-        ${bookTitle}
-        </span>
-            </li>
-            <li class="author-list">
-        <span>
-        ${bookAuthor}
-        </span>
-            </li>
+  bookCollection.forEach((book, index) => {
+    const html = `
+      <article class="book-list">
+        <ul class="book-details">
+          <li class="title-list">
+            <span>${book.title}</span>
+          </li>
+          <li class="author-list">
+            <span>${book.author}</span>
+          </li>
         </ul>
-        <button type="submit" class="remove">Remove</button>
-    </article>
-  `;
+        <button type="submit" class="remove" data-index="${index}">Remove</button>
+      </article>
+    `;
 
-  bookShelf.innerHTML += html;
+    bookShelf.innerHTML += html;
+  });
 };
 
-// add event listener to the form
+renderBooks();
+
 addBook.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const bookTitle = addBook.title.value.trim();
   const bookAuthor = addBook.author.value.trim();
 
-  // check if the input is not empty
   if (bookTitle.length && bookAuthor.length) {
-    generateBook(bookTitle, bookAuthor);
+    bookCollection.push({ title: bookTitle, author: bookAuthor });
+    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+    renderBooks();
 
-    bookCollection.push(bookTitle, bookAuthor);
-    console.log(bookCollection);
-
-
-    // reset the form
     addBook.reset();
   }
 });
 
-// remove the book from the list
 bookShelf.addEventListener('click', (e) => {
   if (e.target.classList.contains('remove')) {
-    e.target.parentElement.remove();
+    const index = e.target.dataset.index;
+    bookCollection.splice(index, 1);
+    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+    renderBooks();
   }
 });
